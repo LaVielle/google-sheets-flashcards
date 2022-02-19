c5aed58f5898fb37d399b2508b36cc5fe20a0e34
Samuel Mann <samuelcmann@gmail.com>
2022-02-19 16:17:40 +0100

    get basic Google auth working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react';
 
+import { useSession, signIn, signOut } from 'next-auth/react';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
+  const { data: session } = useSession();
+  console.log({ session });
   return (
     <>
       <Seo templateTitle='Home' />
 
-      <main className='flex h-screen h-screen w-screen flex-col'></main>
+      <div className={'flex flex-col p-32'}>
+        <button
+          type={'button'}
+          onClick={() => signIn('google')}
+          className={
+            'rounded-md bg-indigo-500 px-8 py-4 text-2xl font-semibold text-white'
+          }
+        >
+          Sign in
+        </button>
+
+        <div className={'h-5'} />
+
+        <button
+          type={'button'}
+          onClick={() => signOut()}
+          className={
+            'rounded-md bg-red-500 px-8 py-4 text-2xl font-semibold text-white'
+          }
+        >
+          Sign out
+        </button>
+      </div>
     </>
   );
 }
