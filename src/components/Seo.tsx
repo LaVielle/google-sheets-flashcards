5ec68629e4af3eed3335630f7684acd7eca7d61f
Samuel Mann <samuelcmann@gmail.com>
2022-02-19 14:14:58 +0100

    first commit

---
+++ src/components/Seo.tsx
@@ -0,0 +1,70 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+// !STARTERCONF Change these default meta
+const defaultMeta = {
+  title: 'Sheets to Spaced Repetition',
+  siteName: 'Sheets to Spaced Repetition',
+  description: 'Spaced Repetition app to review your Google Sheets data',
+  url: 'https://example.com',
+  type: 'website',
+  robots: 'follow, index',
+  image: 'https://example.com',
+};
+
+type SeoProps = {
+  date?: string;
+  templateTitle?: string;
+} & Partial<typeof defaultMeta>;
+
+export default function Seo(props: SeoProps) {
+  const router = useRouter();
+  const meta = {
+    ...defaultMeta,
+    ...props,
+  };
+  meta['title'] = props.templateTitle
+    ? `${props.templateTitle} | ${meta.siteName}`
+    : meta.title;
+
+  return (
+    <Head>
+      <title>{meta.title}</title>
+      <meta name='robots' content={meta.robots} />
+      <meta content={meta.description} name='description' />
+      <meta property='og:url' content={`${meta.url}${router.asPath}`} />
+      <link rel='canonical' href={`${meta.url}${router.asPath}`} />
+      {/* Open Graph */}
+      <meta property='og:type' content={meta.type} />
+      <meta property='og:site_name' content={meta.siteName} />
+      <meta property='og:description' content={meta.description} />
+      <meta property='og:title' content={meta.title} />
+      <meta name='image' property='og:image' content={meta.image} />
+      {/* Twitter */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content='@th_clarence' />
+      <meta name='twitter:title' content={meta.title} />
+      <meta name='twitter:description' content={meta.description} />
+      <meta name='twitter:image' content={meta.image} />
+      {meta.date && (
+        <>
+          <meta property='article:published_time' content={meta.date} />
+          <meta
+            name='publish_date'
+            property='og:publish_date'
+            content={meta.date}
+          />
+          <meta
+            name='author'
+            property='article:author'
+            content='Theodorus Clarence'
+          />
+        </>
+      )}
+
+      <link rel='icon' href='favicon/favicon.ico' type='image/x-icon' />
+
+      <meta name='theme-color' content='#ffffff' />
+    </Head>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Seo from '@/components/Seo';
+
+export default function HomePage() {
+  return (
+    <>
+      <Seo templateTitle='Home' />
+
+      <main className='flex h-screen h-screen w-screen flex-col'></main>
+    </>
+  );
+}
