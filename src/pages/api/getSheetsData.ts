4a2d5b4d0db3a147c928dc658af21bb166b5212b
Samuel Mann <samuelcmann@gmail.com>
2022-02-27 12:03:04 +0100

    get data from google sheets api and return as json

--- src/pages/api/getSheetsData.ts
+++ src/pages/api/getSheetsData.ts
@@ -1,20 +1,51 @@
+import { sheets } from '@googleapis/sheets';
 import { NextApiHandler } from 'next';
 import { getToken } from 'next-auth/jwt';
 
-type GoogleSheetApiResponse = {
-  something: string;
-};
+type GoogleSheetApiResponse =
+  | {
+      data: Record<string, string>[];
+    }
+  | {
+      errorMessage: string;
+    };
 
 const getSheetsData: NextApiHandler<GoogleSheetApiResponse> = async (
   req,
   res
 ) => {
-  const token = await getToken({
-    req,
-    secret: process.env.GFS_SECRET,
-  });
+  try {
+    const token = await getToken({ req });
 
-  res.status(200).json({ something: 'durite' });
+    const sheetsClient = sheets('v4');
+
+    const response = await sheetsClient.spreadsheets.values.get({
+      spreadsheetId: '1WKsGeNHnLZonPRiaysnAfUL3wNsF_oWnIONojSQ5PhM', // German sentences
+      range: 'Sheet1', // sheet name
+      access_token: token?.accessToken as string,
+    });
+
+    if (!response.data.values?.length) {
+      throw new Error('File does not contain translations');
+    }
+    const [languageHeaders, ...translationTuples] = response.data.values;
+
+    const translations =
+      translationTuples.map((translationSet) => {
+        const obj: Record<string, string> = {};
+
+        translationSet.forEach((lng, index) => {
+          obj[languageHeaders[index]] = lng;
+        });
+
+        return obj;
+      }) || [];
+
+    res.status(200).json({ data: translations });
+  } catch (e) {
+    console.log('error', e);
+    res.status(500).json({ errorMessage: e as string });
+  }
 };
 
 export default getSheetsData;
